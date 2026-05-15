1d83ade11e9de5680ba2e1ed52f4a63516297cb2
FFMG <github@myoddweb.com>
2026-05-15 18:36:51 +0200

    Split the tests where it makes more sense.

--- tests/testjson5numbers.cpp
+++ tests/testjson5numbers.cpp
@@ -11,131 +11,197 @@ using namespace TinyJSON;
 // These tests are inspired by the official JSON5 test suite:
 // https://github.com/json5/json5-tests/tree/master/numbers
 
-TEST(TJJSON5Numbers, PlusSign)
+TEST(TJJSON5Numbers, PlusSignInteger)
 {
   parse_options options;
   options.specification = parse_options::json5_1_0_0;
 
-  const TJCHAR* json1 = TJCHARPREFIX("+42");
-  auto* tj1 = TJ::parse(json1, options);
-  ASSERT_NE(nullptr, tj1);
-  EXPECT_EQ(42, tj1->get_number<int>());
-  delete tj1;
-
-  const TJCHAR* json2 = TJCHARPREFIX("+0.5");
-  auto* tj2 = TJ::parse(json2, options);
-  ASSERT_NE(nullptr, tj2);
-  EXPECT_DOUBLE_EQ(0.5, tj2->get_float<double>());
-  delete tj2;
-
-  const TJCHAR* json3 = TJCHARPREFIX("+.5");
-  auto* tj3 = TJ::parse(json3, options);
-  ASSERT_NE(nullptr, tj3);
-  EXPECT_DOUBLE_EQ(0.5, tj3->get_float<double>());
-  delete tj3;
+  const TJCHAR* json = TJCHARPREFIX("+42");
+  auto* tj = TJ::parse(json, options);
+  ASSERT_NE(nullptr, tj);
+  EXPECT_EQ(42, tj->get_number<int>());
+  delete tj;
 }
 
-TEST(TJJSON5Numbers, LeadingTrailingDecimal)
+TEST(TJJSON5Numbers, PlusSignFloat)
 {
   parse_options options;
   options.specification = parse_options::json5_1_0_0;
 
-  const TJCHAR* json1 = TJCHARPREFIX(".5");
-  auto* tj1 = TJ::parse(json1, options);
-  ASSERT_NE(nullptr, tj1);
-  EXPECT_DOUBLE_EQ(0.5, tj1->get_float<double>());
-  delete tj1;
-
-  const TJCHAR* json2 = TJCHARPREFIX("5.");
-  auto* tj2 = TJ::parse(json2, options);
-  ASSERT_NE(nullptr, tj2);
-  EXPECT_DOUBLE_EQ(5.0, tj2->get_float<double>());
-  delete tj2;
+  const TJCHAR* json = TJCHARPREFIX("+0.5");
+  auto* tj = TJ::parse(json, options);
+  ASSERT_NE(nullptr, tj);
+  EXPECT_DOUBLE_EQ(0.5, tj->get_float<double>());
+  delete tj;
 }
 
-TEST(TJJSON5Numbers, Hexadecimal)
+TEST(TJJSON5Numbers, PlusSignFloatLeadingDecimal)
 {
   parse_options options;
   options.specification = parse_options::json5_1_0_0;
 
-  const TJCHAR* json1 = TJCHARPREFIX("0x42");
-  auto* tj1 = TJ::parse(json1, options);
-  ASSERT_NE(nullptr, tj1);
-  EXPECT_EQ(0x42, tj1->get_number<int>());
-  delete tj1;
-
-  const TJCHAR* json2 = TJCHARPREFIX("0XFF");
-  auto* tj2 = TJ::parse(json2, options);
-  ASSERT_NE(nullptr, tj2);
-  EXPECT_EQ(255, tj2->get_number<int>());
-  delete tj2;
-
-  const TJCHAR* json3 = TJCHARPREFIX("+0x1");
-  auto* tj3 = TJ::parse(json3, options);
-  ASSERT_NE(nullptr, tj3);
-  EXPECT_EQ(1, tj3->get_number<int>());
-  delete tj3;
-
-  const TJCHAR* json4 = TJCHARPREFIX("-0x1");
-  auto* tj4 = TJ::parse(json4, options);
-  ASSERT_NE(nullptr, tj4);
-  EXPECT_EQ(-1, tj4->get_number<int>());
-  delete tj4;
+  const TJCHAR* json = TJCHARPREFIX("+.5");
+  auto* tj = TJ::parse(json, options);
+  ASSERT_NE(nullptr, tj);
+  EXPECT_DOUBLE_EQ(0.5, tj->get_float<double>());
+  delete tj;
 }
 
-TEST(TJJSON5Numbers, SpecialValues)
+TEST(TJJSON5Numbers, LeadingDecimal)
 {
   parse_options options;
   options.specification = parse_options::json5_1_0_0;
 
-  const TJCHAR* json1 = TJCHARPREFIX("NaN");
-  auto* tj1 = TJ::parse(json1, options);
-  ASSERT_NE(nullptr, tj1);
-  EXPECT_TRUE(std::isnan(static_cast<double>(tj1->get_float<double>())));
-  delete tj1;
-
-  const TJCHAR* json2 = TJCHARPREFIX("Infinity");
-  auto* tj2 = TJ::parse(json2, options);
-  ASSERT_NE(nullptr, tj2);
-  EXPECT_TRUE(std::isinf(static_cast<double>(tj2->get_float<double>())));
-  EXPECT_GT(tj2->get_float<double>(), 0);
-  delete tj2;
-
-  const TJCHAR* json3 = TJCHARPREFIX("-Infinity");
-  auto* tj3 = TJ::parse(json3, options);
-  ASSERT_NE(nullptr, tj3);
-  EXPECT_TRUE(std::isinf(static_cast<double>(tj3->get_float<double>())));
-  EXPECT_LT(tj3->get_float<double>(), 0);
-  delete tj3;
-
-  const TJCHAR* json4 = TJCHARPREFIX("+Infinity");
-  auto* tj4 = TJ::parse(json4, options);
-  ASSERT_NE(nullptr, tj4);
-  EXPECT_TRUE(std::isinf(static_cast<double>(tj4->get_float<double>())));
-  EXPECT_GT(tj4->get_float<double>(), 0);
-  delete tj4;
+  const TJCHAR* json = TJCHARPREFIX(".5");
+  auto* tj = TJ::parse(json, options);
+  ASSERT_NE(nullptr, tj);
+  EXPECT_DOUBLE_EQ(0.5, tj->get_float<double>());
+  delete tj;
 }
 
-TEST(TJJSON5Numbers, Exponents)
+TEST(TJJSON5Numbers, TrailingDecimal)
 {
   parse_options options;
   options.specification = parse_options::json5_1_0_0;
 
-  const TJCHAR* json1 = TJCHARPREFIX("1e+2");
-  auto* tj1 = TJ::parse(json1, options);
-  ASSERT_NE(nullptr, tj1);
-  EXPECT_DOUBLE_EQ(100.0, tj1->get_float<double>());
-  delete tj1;
+  const TJCHAR* json = TJCHARPREFIX("5.");
+  auto* tj = TJ::parse(json, options);
+  ASSERT_NE(nullptr, tj);
+  EXPECT_DOUBLE_EQ(5.0, tj->get_float<double>());
+  delete tj;
+}
 
-  const TJCHAR* json2 = TJCHARPREFIX(".5e-1");
-  auto* tj2 = TJ::parse(json2, options);
-  ASSERT_NE(nullptr, tj2);
-  EXPECT_DOUBLE_EQ(0.05, tj2->get_float<double>());
-  delete tj2;
+TEST(TJJSON5Numbers, HexadecimalLowerX)
+{
+  parse_options options;
+  options.specification = parse_options::json5_1_0_0;
 
-  const TJCHAR* json3 = TJCHARPREFIX("5.e1");
-  auto* tj3 = TJ::parse(json3, options);
-  ASSERT_NE(nullptr, tj3);
-  EXPECT_DOUBLE_EQ(50.0, tj3->get_float<double>());
-  delete tj3;
+  const TJCHAR* json = TJCHARPREFIX("0x42");
+  auto* tj = TJ::parse(json, options);
+  ASSERT_NE(nullptr, tj);
+  EXPECT_EQ(0x42, tj->get_number<int>());
+  delete tj;
+}
+
+TEST(TJJSON5Numbers, HexadecimalUpperX)
+{
+  parse_options options;
+  options.specification = parse_options::json5_1_0_0;
+
+  const TJCHAR* json = TJCHARPREFIX("0XFF");
+  auto* tj = TJ::parse(json, options);
+  ASSERT_NE(nullptr, tj);
+  EXPECT_EQ(255, tj->get_number<int>());
+  delete tj;
+}
+
+TEST(TJJSON5Numbers, HexadecimalPlusSign)
+{
+  parse_options options;
+  options.specification = parse_options::json5_1_0_0;
+
+  const TJCHAR* json = TJCHARPREFIX("+0x1");
+  auto* tj = TJ::parse(json, options);
+  ASSERT_NE(nullptr, tj);
+  EXPECT_EQ(1, tj->get_number<int>());
+  delete tj;
+}
+
+TEST(TJJSON5Numbers, HexadecimalMinusSign)
+{
+  parse_options options;
+  options.specification = parse_options::json5_1_0_0;
+
+  const TJCHAR* json = TJCHARPREFIX("-0x1");
+  auto* tj = TJ::parse(json, options);
+  ASSERT_NE(nullptr, tj);
+  EXPECT_EQ(-1, tj->get_number<int>());
+  delete tj;
+}
+
+TEST(TJJSON5Numbers, NaN)
+{
+  parse_options options;
+  options.specification = parse_options::json5_1_0_0;
+
+  const TJCHAR* json = TJCHARPREFIX("NaN");
+  auto* tj = TJ::parse(json, options);
+  ASSERT_NE(nullptr, tj);
+  EXPECT_TRUE(std::isnan(static_cast<double>(tj->get_float<double>())));
+  delete tj;
+}
+
+TEST(TJJSON5Numbers, Infinity)
+{
+  parse_options options;
+  options.specification = parse_options::json5_1_0_0;
+
+  const TJCHAR* json = TJCHARPREFIX("Infinity");
+  auto* tj = TJ::parse(json, options);
+  ASSERT_NE(nullptr, tj);
+  EXPECT_TRUE(std::isinf(static_cast<double>(tj->get_float<double>())));
+  EXPECT_GT(tj->get_float<double>(), 0);
+  delete tj;
+}
+
+TEST(TJJSON5Numbers, MinusInfinity)
+{
+  parse_options options;
+  options.specification = parse_options::json5_1_0_0;
+
+  const TJCHAR* json = TJCHARPREFIX("-Infinity");
+  auto* tj = TJ::parse(json, options);
+  ASSERT_NE(nullptr, tj);
+  EXPECT_TRUE(std::isinf(static_cast<double>(tj->get_float<double>())));
+  EXPECT_LT(tj->get_float<double>(), 0);
+  delete tj;
+}
+
+TEST(TJJSON5Numbers, PlusInfinity)
+{
+  parse_options options;
+  options.specification = parse_options::json5_1_0_0;
+
+  const TJCHAR* json = TJCHARPREFIX("+Infinity");
+  auto* tj = TJ::parse(json, options);
+  ASSERT_NE(nullptr, tj);
+  EXPECT_TRUE(std::isinf(static_cast<double>(tj->get_float<double>())));
+  EXPECT_GT(tj->get_float<double>(), 0);
+  delete tj;
+}
+
+TEST(TJJSON5Numbers, ExponentPlus)
+{
+  parse_options options;
+  options.specification = parse_options::json5_1_0_0;
+
+  const TJCHAR* json = TJCHARPREFIX("1e+2");
+  auto* tj = TJ::parse(json, options);
+  ASSERT_NE(nullptr, tj);
+  EXPECT_DOUBLE_EQ(100.0, tj->get_float<double>());
+  delete tj;
+}
+
+TEST(TJJSON5Numbers, ExponentMinusLeadingDecimal)
+{
+  parse_options options;
+  options.specification = parse_options::json5_1_0_0;
+
+  const TJCHAR* json = TJCHARPREFIX(".5e-1");
+  auto* tj = TJ::parse(json, options);
+  ASSERT_NE(nullptr, tj);
+  EXPECT_DOUBLE_EQ(0.05, tj->get_float<double>());
+  delete tj;
+}
+
+TEST(TJJSON5Numbers, ExponentTrailingDecimal)
+{
+  parse_options options;
+  options.specification = parse_options::json5_1_0_0;
+
+  const TJCHAR* json = TJCHARPREFIX("5.e1");
+  auto* tj = TJ::parse(json, options);
+  ASSERT_NE(nullptr, tj);
+  EXPECT_DOUBLE_EQ(50.0, tj->get_float<double>());
+  delete tj;
 }
