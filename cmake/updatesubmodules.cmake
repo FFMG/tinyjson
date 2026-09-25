365a4f428f2727859dab04a0feecd1d34ab18248
FFMG <github@myoddweb.com>
2026-09-25 12:28:00 +0200

    Cleanned up the cmake files

--- cmake/updatesubmodules.cmake
+++ cmake/updatesubmodules.cmake
@@ -1,3 +1,6 @@
+# Licensed to Florent Guelfucci under one or more agreements.
+# Florent Guelfucci licenses this file to you under the MIT license.
+# See the LICENSE file in the project root for more information.
 # Adapted from https://cliutils.gitlab.io/modern-cmake/chapters/projects/submodule.html
 find_package(Git QUIET)
 if(GIT_FOUND)
